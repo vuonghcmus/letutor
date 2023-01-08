b69c10ce53c1a61a0c26fc9ff47464ba32ecb8c6
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2023-01-09 05:30:37 +0700

    remove comment

--- lib/main.dart
+++ lib/main.dart
@@ -16,21 +16,11 @@ void main() async {
   runApp(mainApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(home: LoginPage());
-//   }
-// }
-
-class MyHttpOverrides extends HttpOverrides{
+class MyHttpOverrides extends HttpOverrides {
   @override
-  HttpClient createHttpClient(SecurityContext? context){
+  HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
-
