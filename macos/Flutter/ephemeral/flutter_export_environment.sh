c855fc1b0b6370c373fc970d33ca3e7bea2e1b3c
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2023-01-14 10:29:44 +0700

    revert

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\DAI_HOC\NAM_4\PHAT_TRIEN_UNG_DUNG_CHO_THIET_BI_DI_DONG_NANG_CAO\vuong\letutor"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
